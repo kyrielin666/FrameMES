ac49424747d3230478ee4e9c88a60eb20e4f5922
qrl <631968859@qq.com>
2025-06-24 16:35:19 +0800

    添加项目文件。

---
+++ FrameMES/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using FrameMES.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FrameMES
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+           return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+ 
+}
